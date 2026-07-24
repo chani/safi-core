8dad360b4de65a28d87312c6c3a8b0a732762562
Jean Bruenn <himself@jeanbruenn.info>
2026-07-24 09:30:28 +0100

    bump: release version 0.1.3

--- src/Kernel.php
+++ src/Kernel.php
@@ -23,7 +23,7 @@ use Throwable;
 
 final class Kernel
 {
-    public const string VERSION = '0.1.2';
+    public const string VERSION = '0.1.3';
 
     /** @var array<int, class-string<MiddlewareInterface>|callable|MiddlewareInterface> */
     private array $middlewares = [];
