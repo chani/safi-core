99a1627eda1e7cef9aaef5aac61b4a9e7112c278
Jean Bruenn <himself@jeanbruenn.info>
2026-07-26 02:01:11 +0100

    chore(core): bump Kernel VERSION constant to 0.1.10

--- src/Kernel.php
+++ src/Kernel.php
@@ -25,7 +25,7 @@ use Throwable;
 
 final class Kernel
 {
-    public const string VERSION = '0.1.9';
+    public const string VERSION = '0.1.10';
 
     /** @var array<int, class-string<MiddlewareInterface>|callable|MiddlewareInterface> */
     private array $middlewares = [];
