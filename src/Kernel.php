3b12528396c6ee27966e0d877e3fd56b2639e791
Jean Bruenn <himself@jeanbruenn.info>
2026-07-31 17:13:38 +0100

    chore: bump Kernel VERSION constant to 0.1.16

--- src/Kernel.php
+++ src/Kernel.php
@@ -26,7 +26,7 @@ use Throwable;
 
 final class Kernel
 {
-    public const string VERSION = '0.1.15';
+    public const string VERSION = '0.1.16';
 
     /** @var array<int, callable|MiddlewareInterface> */
     private array $middlewares;
